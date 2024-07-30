3e0f3609c027ff547c65beec60f1217c167834a5
Vikas Raut <119796885+vikas-1234-raut@users.noreply.github.com>
2024-07-30 21:01:31 +0530

    Update globalContext.js

--- Frontend/src/context/globalContext.js
+++ Frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://money-monitor-backend.onrender.com/api/v1/";
+const BASE_URL = "https://money-monitor-backend.onrender.com";
 
 
 const GlobalContext = React.createContext()
